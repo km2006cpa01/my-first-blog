e4deb02e64ad7a91995ad56db9cf5bfbfc16140c
Kenji Matsubara <km2006cpa01@gmail.com>
2020-07-29 00:25:56 +0900

    additional files and changes for Heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,9 +24,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'v!k1(f8col50-d78u-7@krnc0_d20u+lglig-=a6w$u#xw#21^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+# ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
@@ -75,10 +78,29 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+
+    #'default': {
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': 'djangogirls',
+        # 'USER': 'postgres',
+        # 'PASSWORD': 'djangogirls',
+        # 'HOST': '127.0.0.1',
+        # 'PORT': '5432',
+    # }
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_spycopg2',
+        'NAME': 'djangogirls',
+        'USER': 'km2006cpa01',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
+
 }
 
 
@@ -120,3 +142,6 @@ USE_TZ = True
 #
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
